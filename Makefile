c95fbca683b931878bba357d8937692ec44951bf
sadielise <sadiet@rams.colostate.edu>
2016-11-10 21:52:02 -0700

    setting up makefile and skeletons for router and manager

--- Makefile
+++ Makefile
@@ -1,1 +1,12 @@
-
+CXXFLAGS=-Wall -g -I.
+all: router manager
+router:router.o
+	g++ $(CXXFLAGS) router.o -o router
+manager:manager.o
+	g++ $(CXXFLAGS) manager.o -o manager
+router.o:router.cpp
+	g++ $(CXXFLAGS) -c router.cpp
+manager.o:manager.cpp
+	g++ $(CXXFLAGS) -c manager.cpp
+clean:
+	rm -f *.o router manager
--- router.cpp
+++ router.cpp
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <limits.h>
+#include <netdb.h>
+#include <fstream>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+#include <ctime>
+#include <signal.h>
 
+using namespace std;
+using namespace boost;
+
+void sig_handler(int signal){
+	exit(0);
+}
+
+int main(int argc, char* argv[]) {
+
+		// handle signals
+		signal(SIGINT/SIGTERM/SIGKILL, sig_handler);
+
+    return 0;
+}
