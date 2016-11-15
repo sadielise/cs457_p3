62183a066cbe4901ca1e500d82560f5e6c781727
sadielise <sadiet@rams.colostate.edu>
2016-11-14 21:33:31 -0700

    getting there, but need to get connections working

--- manager.cpp
+++ manager.cpp
@@ -25,9 +25,18 @@
 using namespace std;
 using namespace boost;
 
+#define PORT_NUMBER 20001
+#define MESSAGE_SIZE 140
+#define VERSION 457
 int NUM_NODES;
 int DEBUG = 1;
 int MAX_CONNECTION_LENGTH = 8;
+
+struct packet{
+	short version;
+	short length;
+	char message[MESSAGE_SIZE];
+};	
 
 int printHelpMessage() {
     cout << endl << "manager help:" << endl << endl;
@@ -82,6 +91,80 @@ void childProcess(pid_t pid, int fd){
 	write(fd, temp.c_str(), temp.size());
 }
 
+int run_server()
+{
+	char host_name[HOST_NAME_MAX]; //character array for host name
+	gethostname(host_name, sizeof(host_name)); //read host name into char array
+	struct hostent* host_info = gethostbyname(host_name); //get IP address
+	struct sockaddr_in server_address = {}; //initialize server sockaddr structure
+	server_address.sin_family = AF_INET; //set family
+	server_address.sin_port = htons(PORT_NUMBER); //convert and set port
+	bcopy(host_info->h_addr, (char*)&server_address.sin_addr, host_info->h_length); //set ip address
+
+	char ip_address[INET_ADDRSTRLEN]; //character array for ip address
+	inet_ntop(AF_INET, &server_address.sin_addr, ip_address, sizeof(ip_address));	//convert binary IP to string
+	printf("Welcome to Chat!\n");
+	printf("Waiting for a connection on %s port %i\n", ip_address, PORT_NUMBER);
+
+	int file_descriptor = socket(AF_INET, SOCK_STREAM, 0); //create socket
+	if(file_descriptor == -1){
+		printf("Error: Could not create server socket.\n");
+		return -1;
+	}
+
+	int bind_result = bind(file_descriptor, (struct sockaddr*)&server_address, sizeof(server_address)); //bind socket to port
+	if(bind_result == -1){
+		printf("Error: Could not bind server socket.\n");
+		return -1;
+	}
+
+	int listen_result = listen(file_descriptor, 1); //start listening
+	if(listen_result == -1){
+		printf("Error: Could not begin listening.\n");
+		return -1;
+	}
+
+	socklen_t server_address_len = sizeof(server_address); //get length of socket
+	int connection_descriptor = accept(file_descriptor, (struct sockaddr*)&server_address, &server_address_len); //connect to client
+	if(connection_descriptor == -1){
+		printf("Error: Could not accept.\n");
+		return -1;
+	}
+
+	struct packet receive_packet = {}; //buffer to receive message
+	char* receive_array = reinterpret_cast<char*>(&receive_packet); //cast the packet to a char* so that it can be used by recv
+	int receive_result = recv(connection_descriptor, receive_array, sizeof(receive_packet), 0); //receive message
+	receive_packet.version = ntohs(receive_packet.version); //change version back to host byte order
+	receive_packet.length = ntohs(receive_packet.length); //change length back to host by order
+	if(receive_result == -1){
+		printf("Error: Could not receive from client.\n");
+		return -1;
+	}
+	printf("Client: %s\n", receive_packet.message); //print message from client
+
+	char send_buffer[MESSAGE_SIZE]; //buffer to send message
+	memset(send_buffer, 0, sizeof(send_buffer)); //set the buffer to zeros
+	string temp = "hello from the server";
+	for(unsigned int i = 0; i < temp.size(); i++){ //copy string into char array
+		send_buffer[i] = temp.at(i);
+	}
+	struct packet send_packet = {}; //create packet to send
+	send_packet.version = htons(VERSION); //set version
+	send_packet.length = htons(temp.length()); //set length
+	bcopy(send_buffer, send_packet.message, sizeof(send_packet.message)); //set message
+	const char* send_array = reinterpret_cast<const char*>(&send_packet);
+	int send_result = send(connection_descriptor, send_array, sizeof(send_packet), 0); //send message
+	if(send_result == -1){
+		printf("Error: Could not send to server.\n");
+		return -1;
+	}
+	temp.clear(); //clear the temp string
+		
+	close(connection_descriptor);
+	close(file_descriptor);
+	return 0;
+}
+
 void sig_handler(int signal){
 	exit(0);
 }
@@ -106,13 +189,13 @@ int main(int argc, char* argv[]) {
 
 	pid_t pids[NUM_NODES];
 	cout << "hello world parent" << endl;
-	parentProcess(getpid(), fd);
+	run_server();
 
 	for(int i = 0; i < NUM_NODES; i++){
 		pids[i] = fork();
 		if(pids[i] == 0){
 			cout << "hello world child" << endl;
-			childProcess(pids[i], fd);
+			system("./router");
 			_exit(0);
 		}
 		else if(pids[i] < 0){
--- router.cpp
+++ router.cpp
@@ -21,11 +21,72 @@
 using namespace std;
 using namespace boost;
 
-void childProcess(pid_t pid, int fd){
-	int pidChild = getpid();
-	string pidVal = to_string(pidChild);
-	string temp = "child process " + pidVal + "\n";
-	write(fd, temp.c_str(), temp.size());
+#define PORT_NUMBER 20001
+#define MESSAGE_SIZE 140
+#define VERSION 457
+int DEBUG = 1;
+
+struct packet{
+	short version;
+	short length;
+	char message[MESSAGE_SIZE];
+};	
+
+int run_client(int port, char* ip)
+{
+	struct sockaddr_in client_address = {}; //initialize client sockaddr structure
+	client_address.sin_family = AF_INET; //set family
+	client_address.sin_port = htons(port); //convert and set port
+	struct in_addr in_addr_temp = {}; //setup in_addr for ip number
+	inet_pton(AF_INET, ip, &in_addr_temp); //set s_addr in in_addr_temp
+	client_address.sin_addr = in_addr_temp; //add in_addr to sockaddr_in
+
+	int file_descriptor = socket(AF_INET, SOCK_STREAM, 0); //create socket
+	if(file_descriptor == -1){
+		printf("Error: Could not create server socket.\n");
+		return -1;
+	}
+
+	int connect_result = connect(file_descriptor, (struct sockaddr*)&client_address, sizeof(client_address)); //connect to server
+	if(connect_result == -1){
+		close(file_descriptor);			
+		printf("\nError: Could not connect to server.\n");
+		return -1;
+	}
+
+	printf("Connected!\n"); 
+
+	char send_buffer[MESSAGE_SIZE]; //buffer to send message
+	memset(send_buffer, 0, sizeof(send_buffer)); //set the buffer to zeros
+	string temp = "hello from client";
+	for(unsigned int i = 0; i < temp.size(); i++){ //copy string into char array
+		send_buffer[i] = temp.at(i);
+	}
+	struct packet send_packet = {}; //create packet to send
+	send_packet.version = htons(VERSION); //set version
+	send_packet.length = htons(temp.length()); //set length
+	bcopy(send_buffer, send_packet.message, sizeof(send_packet.message)); //set message
+	const char* send_array = reinterpret_cast<const char*>(&send_packet);
+	int send_result = send(file_descriptor, send_array, sizeof(send_packet), 0); //send message
+	if(send_result == -1){
+		printf("Error: Could not send to server.\n");
+		return -1;
+	}
+	temp.clear(); //clear the temp string
+
+	struct packet receive_packet = {}; //buffer to receive message
+	char* receive_array = reinterpret_cast<char*>(&receive_packet); //cast the packet to a char* so that it can be used by recv
+	int receive_result = recv(file_descriptor, receive_array, sizeof(receive_packet), 0); //receive message
+	receive_packet.version = ntohs(receive_packet.version); //change version back to host byte order
+	receive_packet.length = ntohs(receive_packet.length); //change length back to host by order
+	if(receive_result == -1){
+		printf("Error: Could not receive from client.\n");
+		return -1;
+	}
+		printf("Server: %s\n", receive_packet.message); //print message from client
+	
+	close(file_descriptor);
+	return 0;
 }
 
 void sig_handler(int signal){
@@ -36,6 +97,10 @@ int main(int argc, char* argv[]) {
 
 	// handle signals
 	signal(SIGINT/SIGTERM/SIGKILL, sig_handler);
+
+	cout << "got to the client" << endl;
+	char ip[] = "129.82.44.134";
+	run_client(PORT_NUMBER, ip);
 
     return 0;
 }
